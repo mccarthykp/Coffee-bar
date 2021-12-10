d51a7e3f57c960c2762f5e724a1c6fe7bda197f2
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-12-10 17:05:43 -0500

    imported Person class and placed orders at coffee bar

--- coffee_bar.py
+++ coffee_bar.py
@@ -1,3 +1,5 @@
+from person import Amy, Bob, Cat
+
 class CoffeeBar():
   def __init__(self, name):
     self.name = name
@@ -9,3 +11,11 @@ class CoffeeBar():
   def process_orders(self):
     for order in self.order_list:
       print(order.to_string())
+
+
+if __name__ == "__main__":
+  CoffeeEmporium = CoffeeBar('Coffee Emporium')
+
+  CoffeeEmporium.place_order(Amy.my_order())
+  CoffeeEmporium.place_order(Bob.my_order())
+  CoffeeEmporium.place_order(Cat.my_order())
