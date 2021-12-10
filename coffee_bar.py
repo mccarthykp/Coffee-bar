8b8946cd666958db0c1036c1e3dd18a698044cbb
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-12-10 17:19:21 -0500

    added barista attribute to coffee_barclass, assigned barista instance to coffee_bar instance

--- coffee_bar.py
+++ coffee_bar.py
@@ -2,9 +2,10 @@ from barista import Barista
 from person import Person
 
 class CoffeeBar():
-  def __init__(self, name):
+  def __init__(self, name, barista):
     self.name = name
     self.order_list = []
+    self.barista = barista
 
   def place_order(self, order):
     self.order_list.append(order)
@@ -19,7 +20,7 @@ if __name__ == "__main__":
   Bob = Person('Bob', 'Tea')
   Cat = Person('Cat', 'Milk')
   Kevin = Barista('Kevin', 'Cold Brew', 'Hey dude!')
-  CoffeeEmporium = CoffeeBar('Coffee Emporium')
+  CoffeeEmporium = CoffeeBar('Coffee Emporium', Kevin)
 
   CoffeeEmporium.place_order(Amy.my_order())
   CoffeeEmporium.place_order(Bob.my_order())
