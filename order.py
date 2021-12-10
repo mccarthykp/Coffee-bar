2646b16a4b5d69f5951052d850267c4b7ff55945
Kevin McCarthy <35618779+mccarthykp@users.noreply.github.com>
2021-12-10 16:58:21 -0500

    created three instances of Person class

--- order.py
+++ order.py
@@ -4,5 +4,5 @@ class Order():
     self.person = person
 
   def to_string(self):
-    order_string = f'{self.person.name} orders: {self.drink_type}'
+    order_string = f'{self.person} orders: {self.drink_type}'
     return order_string
--- person.py
+++ person.py
@@ -1,9 +1,18 @@
+from order import Order
+
 class Person():
   def __init__(self, name, favorite_drink):
     self.name = name
     self.favorite_drink = favorite_drink
 
   def my_order(self):
-    pass
+    order = Order('Coffee', self.name)
+    return order.to_string()
 
+if __name__ == "__main__":
+  Amy = Person('Amy', 'Coffee')
+  Bob = Person('Bob', 'Tea')
+  Cat = Person('Cat', 'Milk')
+
+  print(Bob.my_order())
   
